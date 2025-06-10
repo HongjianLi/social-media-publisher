2f9097a1a436a711ed6929566037f74d627e1c24
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-06-10 16:41:50 +0800

    Run kuaishou.js and weibo.js in start.sh

--- start.sh
+++ start.sh
@@ -7,8 +7,12 @@ echo "$(date +"%F %T.%N") node login.js"
 node login.js
 echo "$(date +"%F %T.%N") node douyin.js"
 node douyin.js
+echo "$(date +"%F %T.%N") node kuaishou.js"
+node kuaishou.js
 echo "$(date +"%F %T.%N") node xiaohongshu.js"
 node xiaohongshu.js
+echo "$(date +"%F %T.%N") node weibo.js"
+node weibo.js
 echo "$(date +"%F %T.%N") node qzone.js"
 node qzone.js
 echo "$(date +"%F %T.%N") Script completed"
