1cac68766b4259d326f2ccf676897dfa463f7a45
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-07-10 15:29:34 +0800

    Support user declaration for douyin and xiaohongshu.

--- douyin.js
+++ douyin.js
@@ -12,10 +12,33 @@ browse('https://creator.douyin.com/creator-micro/content/publish-media/image-tex
 		page.waitForSelector('div.info-jvSF_5', { timeout: 8000 * media.fileArr.length }), // When upload completes, <div class="info-jvSF_5"> will be shown.
 		fileChooser.accept(media.fileArr),
 	]);
-	await page.click('div.select-Ht3mEC');
+	await page.click('div.container-JweCrL>div>label:last-of-type>input'); // 保存权限 不允许
+	await page.click('p.addUserDeclaration-dq21tU'); // 添加声明
+	await new Promise(resolve => setTimeout(resolve, 2000));
+	await page.click('div.radioEl-QlABcQ'); // 内容自行拍摄
 	await new Promise(resolve => setTimeout(resolve, 1000));
-	await page.type('div.select-Ht3mEC', `${media.province}${media.city}${media.district}`);
-	await new Promise(resolve => setTimeout(resolve, 7000));
+	await page.click('section.radioExtra-BUxspN>div.semi-cascader>div>span'); // 选择拍摄地点. Neither page.type() nor page.$eval() worked.
+	await new Promise(resolve => setTimeout(resolve, 1000));
+	await page.click('div.semi-cascader-option-lists>ul>li'); // 第一个ul是国家列表。点击第一个li，中国
+	await new Promise(resolve => setTimeout(resolve, 1000));
+	const provinceArr = await page.$eval('div.semi-cascader-option-lists>ul:nth-child(2)', el => el.innerText); // 第二个ul是省份或直辖市列表
+	const provincenth = provinceArr.split('\n').indexOf(media.province.length ? media.province : media.city);
+	await page.click(`div.semi-cascader-option-lists>ul:nth-child(2)>li:nth-child(${1 + provincenth})`); // nth-child is 1-based.
+	if (media.province.length) { // 若省份不为空，表示仍需继续选择城市
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		const cityArr = await page.$eval('div.semi-cascader-option-lists>ul:nth-child(3)', el => el.innerText);// 第三个ul是城市
+		const citynth = cityArr.split('\n').indexOf(media.city.length ? media.city : media.district);
+		await page.click(`div.semi-cascader-option-lists>ul:nth-child(3)>li:nth-child(${1 + citynth})`); // nth-child is 1-based.
+	}
+	await new Promise(resolve => setTimeout(resolve, 1000));
+	await page.type('section.radioExtra-BUxspN>div.semi-datepicker>div>div>input', `${media.date.substring(0, 4)}-${media.date.substring(4, 6)}-${media.date.substring(6, 8)}`); // 设置拍摄日期
+	await new Promise(resolve => setTimeout(resolve, 1000));
+	await page.click('button.semi-button-primary.btn-I78nOi'); // 确定
+	await new Promise(resolve => setTimeout(resolve, 1000));
+	await page.click('div.select-Ht3mEC'); // 输入相关位置
+	await new Promise(resolve => setTimeout(resolve, 1000));
+	await page.type('div.select-Ht3mEC', `${media.province}${media.city}${media.district}`); // 输入相关位置
+	await new Promise(resolve => setTimeout(resolve, 7000)); // 等待加载位置
 	const cityIndex = (await page.$eval('div#scrollContainer', el => el.innerText)).split('\n').indexOf(media.city); // Custom location is not supported.
 	await page.click(`div#scrollContainer>div:nth-child(${cityIndex >= 0 ? 1 + cityIndex : 1})`); // nth-child is 1-based. Default to the first city if not found.
 	await new Promise(resolve => setTimeout(resolve, 3000));
--- xiaohongshu.js
+++ xiaohongshu.js
@@ -17,6 +17,22 @@ browse('https://creator.xiaohongshu.com/publish/publish', async (page, media) =>
 	await page.type('div.address-box div.d-select-input-filter>input', `${media.province}${media.city}${media.district}`); // Custom location is supported.
 	await new Promise(resolve => setTimeout(resolve, 3000));
 	await page.click('div[data-v-09078844].item');
+	await page.click('span[data-v-190a8d2a].btn-text.red'); // 去声明
+	await page.click('div[data-v-190a8d2a].d-checkbox'); // 我已阅读并同意 《原创声明须知》 ，如滥用声明，平台将驳回并予以相关处置
+	await page.click('button[data-v-365a1a06][data-v-190a8d2a]'); // 声明原创
+	await page.click('div[data-v-190a8d2a].d-select-wrapper'); // 添加内容类型声明
+	await new Promise(resolve => setTimeout(resolve, 3000));
+	await page.click('div.d-popover.d-popover-default.d-dropdown.--size-min-width-large>div.d-dropdown-wrapper>div.d-dropdown-content>div.d-options-wrapper>div.d-grid.d-options>div.d-grid-item:nth-child(7)'); // 内容来源声明
+	await new Promise(resolve => setTimeout(resolve, 3000));
+	await page.click('div.d-popover.d-popover-default.d-dropdown.--size-min-width-large:has(+ div.el-overlay)>div.d-dropdown-wrapper>div.d-dropdown-content>div.d-options-wrapper>div.d-grid.d-options>div.d-grid-item:nth-child(6)'); // 自主拍摄
+	await new Promise(resolve => setTimeout(resolve, 2000 ));
+	await page.type('input[placeholder="下拉选择地点"]', `${media.province}${media.city}${media.district}`)
+	await new Promise(resolve => setTimeout(resolve, 4000 ));
+	await page.click('div[id^="el-popper-container-"] ul>li'); // 选择第一个地点
+	await new Promise(resolve => setTimeout(resolve, 1000 ));
+	await page.type('input[placeholder="下拉选择日期"]', `${media.date.substring(0, 4)}-${media.date.substring(4, 6)}-${media.date.substring(6, 8)}`)
+	await new Promise(resolve => setTimeout(resolve, 1000 ));
+	await page.click('button[data-v-2415ab70].el-button--primary'); // 确认
 	await new Promise(resolve => setTimeout(resolve, 4000 * media.fileArr.length ));
 	await Promise.all([
 		page.waitForNavigation({ waitUntil: 'networkidle2' }),
