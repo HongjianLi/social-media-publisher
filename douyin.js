792e2fec42b04e3f10747a510543706585e04c40
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-07-29 11:24:43 +0800

    Check date before adding a user declaration.

--- douyin.js
+++ douyin.js
@@ -13,28 +13,30 @@ browse('https://creator.douyin.com/creator-micro/content/publish-media/image-tex
 		fileChooser.accept(media.fileArr),
 	]);
 	await page.click('div.container-JweCrL>div>label:last-of-type>input'); // 保存权限 不允许
-	await page.click('p.addUserDeclaration-dq21tU'); // 添加声明
-	await new Promise(resolve => setTimeout(resolve, 2000));
-	await page.click('div.radioEl-QlABcQ'); // 内容自行拍摄
-	await new Promise(resolve => setTimeout(resolve, 1000));
-	await page.click('section.radioExtra-BUxspN>div.semi-cascader>div>span'); // 选择拍摄地点. Neither page.type() nor page.$eval() worked.
-	await new Promise(resolve => setTimeout(resolve, 1000));
-	await page.click('div.semi-cascader-option-lists>ul>li'); // 第一个ul是国家列表。点击第一个li，中国
-	await new Promise(resolve => setTimeout(resolve, 1000));
-	const provinceArr = await page.$eval('div.semi-cascader-option-lists>ul:nth-child(2)', el => el.innerText); // 第二个ul是省份或直辖市列表
-	const provincenth = provinceArr.split('\n').indexOf(media.province.length ? media.province : media.city);
-	await page.click(`div.semi-cascader-option-lists>ul:nth-child(2)>li:nth-child(${1 + provincenth})`); // nth-child is 1-based.
-	if (media.province.length) { // 若省份不为空，表示仍需继续选择城市
+	if (media.date.substring(0, 4) >= '2020') {
+		await page.click('p.addUserDeclaration-dq21tU'); // 添加声明
+		await new Promise(resolve => setTimeout(resolve, 2000));
+		await page.click('div.radioEl-QlABcQ'); // 内容自行拍摄
 		await new Promise(resolve => setTimeout(resolve, 1000));
-		const cityArr = await page.$eval('div.semi-cascader-option-lists>ul:nth-child(3)', el => el.innerText);// 第三个ul是城市
-		const citynth = cityArr.split('\n').indexOf(media.city.length ? media.city : media.district);
-		await page.click(`div.semi-cascader-option-lists>ul:nth-child(3)>li:nth-child(${1 + citynth})`); // nth-child is 1-based.
+		await page.click('section.radioExtra-BUxspN>div.semi-cascader>div>span'); // 选择拍摄地点. Neither page.type() nor page.$eval() worked.
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		await page.click('div.semi-cascader-option-lists>ul>li'); // 第一个ul是国家列表。点击第一个li，中国
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		const provinceArr = await page.$eval('div.semi-cascader-option-lists>ul:nth-child(2)', el => el.innerText); // 第二个ul是省份或直辖市列表
+		const provincenth = provinceArr.split('\n').indexOf(media.province.length ? media.province : media.city);
+		await page.click(`div.semi-cascader-option-lists>ul:nth-child(2)>li:nth-child(${1 + provincenth})`); // nth-child is 1-based.
+		if (media.province.length) { // 若省份不为空，表示仍需继续选择城市
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			const cityArr = await page.$eval('div.semi-cascader-option-lists>ul:nth-child(3)', el => el.innerText);// 第三个ul是城市
+			const citynth = cityArr.split('\n').indexOf(media.city.length ? media.city : media.district);
+			await page.click(`div.semi-cascader-option-lists>ul:nth-child(3)>li:nth-child(${1 + citynth})`); // nth-child is 1-based.
+		}
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		await page.type('section.radioExtra-BUxspN>div.semi-datepicker>div>div>input', `${media.date.substring(0, 4)}-${media.date.substring(4, 6)}-${media.date.substring(6, 8)}`); // 设置拍摄日期 This date has to be newer than 2020-01-01, otherwise it will not be saved.
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		await page.click('button.semi-button-primary.btn-I78nOi'); // 确定
+		await new Promise(resolve => setTimeout(resolve, 1000));
 	}
-	await new Promise(resolve => setTimeout(resolve, 1000));
-	await page.type('section.radioExtra-BUxspN>div.semi-datepicker>div>div>input', `${media.date.substring(0, 4)}-${media.date.substring(4, 6)}-${media.date.substring(6, 8)}`); // 设置拍摄日期 This date has to be newer than 2020-01-01, otherwise it will not be saved.
-	await new Promise(resolve => setTimeout(resolve, 1000));
-	await page.click('button.semi-button-primary.btn-I78nOi'); // 确定
-	await new Promise(resolve => setTimeout(resolve, 1000));
 	await page.click('div.select-Ht3mEC'); // 输入相关位置
 	await new Promise(resolve => setTimeout(resolve, 1000));
 	await page.type('div.select-Ht3mEC', `${media.province}${media.city}${media.district}`); // 输入相关位置
