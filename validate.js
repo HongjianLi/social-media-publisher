68a600936d3cbffdec48a1f716e0282e303aad07
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-08-01 20:33:55 +0800

    Reorder some assertions.

--- validate.js
+++ validate.js
@@ -5,18 +5,18 @@ console.log(mediaArr.length);
 const countArr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 mediaArr.forEach(media => {
 	console.log(media.date, media.weekday, media.country, media.province, media.city, media.district, media.town, media.fileArr.length);
-	console.assert(media.country.length);
-	console.assert(media.province.length || media.city.length); // 例如，重庆是直辖市, province === '' && city === '重庆'
-	console.assert(media.city.length || media.district); // 例如，文昌是海南省县级市, city === '' && district === '文昌'
-	media.title = `${media.date}${media.weekday}${media.country === '中国' ? media.province : media.country}${media.city}${media.district.length ? media.district : media.town}`;
-	console.assert(media.title.length <= 20, media.title); // Max 20 characters for douyin, xiaohongshu.
 	console.assert(media.fileArr.length);
 	console.assert(media.latitude !== undefined);
 	console.assert(media.altitude <= 99999);
+	console.assert(media.country.length);
+	console.assert(media.province.length || media.city.length); // 例如，重庆是直辖市, province === '' && city === '重庆'
+	console.assert(media.city.length || media.district); // 例如，文昌是海南省县级市, city === '' && district === '文昌'
 	console.assert(media.district.length <= 5);
 	console.assert(media.town.length <= 5);
 	console.assert(!media.town.endsWith('镇') || media.town === '古镇', media.town);
 	console.assert(!media.town.endsWith('街道'));
+	media.title = `${media.date}${media.weekday}${media.country === '中国' ? media.province : media.country}${media.city}${media.district.length ? media.district : media.town}`;
+	console.assert(media.title.length <= 20, media.title); // Max 20 characters for douyin, xiaohongshu.
 	const { poem } = media.description;
 	console.assert(poem.length === 4, 'poem.length === 4', poem.length);
 	poem.forEach(sentence => console.assert(sentence.length === 8, 'sentence.length === 8', sentence.length, sentence));
