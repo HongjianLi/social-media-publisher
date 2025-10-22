6b5731c17aaef9a4686909d6b669ac9a7cb5b1e7
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-10-22 16:34:58 +0800

    Delete some await new Promise(resolve => setTimeout(resolve, 1000));

--- kuaishou.js
+++ kuaishou.js
@@ -11,15 +11,11 @@ browse('https://cp.kuaishou.com/article/publish/video?tabType=2', 31, async (pag
 	await page.waitForSelector('div._description_eho7l_59');
 	await page.type('div._description_eho7l_59', `${media.title}\n🌲\n${media.description}`); // Max 500 characters
 	await page.click('input#rc_select_1'); // 添加地点
-	await new Promise(resolve => setTimeout(resolve, 6000));
+	await new Promise(resolve => setTimeout(resolve, 3000));
 	await page.click('ul.ant-cascader-menu>li:nth-child(1)'); // Select the first city.
-	await new Promise(resolve => setTimeout(resolve, 1000));
 	await page.click('div._icon-add_3a3lq_27'); // 添加音乐
-	await new Promise(resolve => setTimeout(resolve, 2000));
 	await page.type('input._search-input_19mmt_16', '红眼睛 陈慧琳');
-	await new Promise(resolve => setTimeout(resolve, 2000));
 	await page.waitForSelector('span._button_19mmt_162'); // 等待添加按钮
-	await new Promise(resolve => setTimeout(resolve, 1000));
 	await page.click('span._button_19mmt_162'); // 添加
 	await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for the music side panel to close.
 	await page.waitForSelector('div._upload_swpvr_1', { timeout: 8000 * (2 + media.fileArr.length) }), // When upload completes, this div 编辑图片 will be shown.
