d09f66a6437171d8bdaeca14ef24c331cbb313bd
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-01-01 17:53:08 +0800

    Support updating multiple cookies for a single site in login.js

--- login.js
+++ login.js
@@ -12,57 +12,63 @@ await browser.setCookie(...cookies);
 const siteArr = [{
 	url: 'https://creator.douyin.com/creator-micro/content/publish-media/image-text',
 	selector: 'span.selected-w_E01s',
-	cookie: {
+	cookies: [{
 		name: 'sessionid',
 		domain: '.douyin.com',
-	},
+	}],
 }, {
 	url: 'https://mp.toutiao.com/profile_v4/weitoutiao/publish',
-	cookie: {
+	cookies: [{
 		name: 'sessionid',
 		domain: '.toutiao.com',
-	},
+	}],
 }, {
 	url: 'https://cp.kuaishou.com/article/publish/video?tabType=2',
 	selector: 'a.login',
-	cookie: {
+	cookies: [{
 		name: 'kuaishou.web.cp.api_st',
 		domain: '.kuaishou.com',
-	},
+	}],
 }, {
 	url: 'https://creator.xiaohongshu.com/publish/publish?target=image',
-	cookie: {
+	cookies: [{
 		name: 'access-token-creator.xiaohongshu.com',
 		domain: '.xiaohongshu.com',
-	},
+	}],
 }, {
 	url: 'https://weibo.com/',
-	cookie: {
+	cookies: [{
 		name: 'SUB',
 		domain: '.weibo.com',
-	},
+	}],
 }, {
 	url: 'https://member.bilibili.com/platform/upload/text/edit',
-	cookie: {
+	cookies: [{
 		name: 'SESSDATA',
 		domain: '.bilibili.com',
-	},
+	}, {
+		name: 'bili_jct',
+		domain: '.bilibili.com',
+	}],
 }, {
 	url: 'https://user.qzone.qq.com/439629497/311',
-	cookie: {
+	cookies: [{
 		name: 'p_skey',
 		domain: '.qzone.qq.com',
-	},
+	}],
 }];
 for (const site of siteArr) {
-	const { url, cookie: { name, domain } } = site;
 	const page = await browser.newPage();
-	const response = await page.goto(url, { waitUntil: 'networkidle2' });
+	const response = await page.goto(site.url, { waitUntil: 'networkidle2' });
 	if (response.ok()) {
-		if (page.url() !== url || (site.selector && await page.$(site.selector))) { // Page redirected because of invalid cookies for user login. douyin and kuaishou will not redirect, but login selectors will be found.
+		if (page.url() !== site.url || (site.selector && await page.$(site.selector))) { // Page redirected because of invalid cookies for user login. douyin and kuaishou will not redirect, but login selectors will be found.
 			await page.waitForNavigation({ timeout: 60000 }); // Scan QR code to login. Default timeout is 30 seconds.
-			const cookie = await browser.cookies().then(cookies => cookies.find(cookie => cookie.name === name && cookie.domain === domain)); // After login, find the credential cookie.
-			if (cookie) cookies.find(cookie => cookie.name === name && cookie.domain === domain).value = cookie.value; // Save the credential cookie value.
+			const browserCookies = await browser.cookies(); // Get the updated cookies from browser.
+			site.cookies.forEach(c => {
+				const { name, domain } = c;
+				const cookie = browserCookies.find(cookie => cookie.name === name && cookie.domain === domain); // After login, find the credential cookie.
+				if (cookie) cookies.find(cookie => cookie.name === name && cookie.domain === domain).value = cookie.value; // Save the credential cookie value.
+			});
 		}
 	} else {
 		console.error(`HTTP response status code ${response.status()}`);
