6a090e55a0350b4d6b7ec4ad2332f855c4664661
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-01-05 09:09:08 +0800

    Add code to solve geetest captcha, but ERROR_CAPTCHA_UNSOLVABLE was returned.

--- bilibili.js
+++ bilibili.js
@@ -22,5 +22,34 @@ browse('https://member.bilibili.com/platform/upload/text/edit', 20, async (page,
 		}, 1000);
 	});
 	await frame.click('button.primary'); // 提交文章
-	await new Promise(resolve => setTimeout(resolve, 500)); // Sometimes captcha is required.
+	// GeeTest captcha v3 is required. https://2captcha.com/demo/geetest  https://2captcha.com/api-docs/geetest
+	await frame.waitForSelector('script[src^="https://api.geetest.com/get.php"]');
+	const geetestParams = await frame.$eval('script[src^="https://api.geetest.com/get.php"]', el => el.src.split('?')[1]).then(search => new URLSearchParams(search));
+	const inRes = await fetch(`https://2captcha.com/in.php?key=${process.env.TWOCAPTCHA_API_KEY}&method=geetest&gt=${geetestParams.get('gt')}&challenge=${geetestParams.get('challenge')}&api_server=api.geetest.com&pageurl=https://member.bilibili.com/platform/upload/text/edit&json=1`);
+	const inRet = await inRes.json();
+	if (!inRet.status) { // status === 0 indicates failure, e.g. when inRet.request === 'ERROR_ZERO_BALANCE' or "ERROR_NO_SLOT_AVAILABLE" or "ERROR_IP_NOT_ALLOWED".
+		throw new Error(`${inRet.request}: ${inRet.error_text}`);
+	}
+	await new Promise(resolve => setTimeout(resolve, 25000)); // Wait for 25 seconds for the captcha to be solved.
+	const resRet = await new Promise((resolve, reject) => {
+		const polling = setInterval(async () => {
+			const resRes = await fetch(`https://2captcha.com/res.php?key=${process.env.TWOCAPTCHA_API_KEY}&action=get&id=${inRet.request}&json=1`);
+			const resRet = await resRes.json();
+			if (resRet.request === 'CAPCHA_NOT_READY') return;
+			clearInterval(polling);
+			resolve(resRet);
+		}, 5000); // If captcha is not solved yet server will return CAPCHA_NOT_READY result. Repeat your request in 5 seconds.
+	});
+	if (!resRet.status) { // status === 0 indicates failure, e.g. when resRet.request === 'ERROR_CAPTCHA_UNSOLVABLE'.
+		throw new Error(`${resRet.request}: ${resRet.error_text}`); // e.g. ERROR_CAPTCHA_UNSOLVABLE: Workers could not solve the Captcha
+	}
+//	Inject these answers [ 'geetest_challenge', 'geetest_validate', 'geetest_seccode' ]
+//	Looks like the answer is returned to the server by creating a new script tag <script src="https://api.geetest.com/ajax.php?gt=&challenge=&w=">
+	await page.$eval('#geetest_validate', (el, answer) => {
+		el.value = answer; // The answer is not guaranteed to be correct.
+	}, resRet.request);
+	await Promise.all([
+		page.waitForNavigation({ timeout: 9000 }),
+		frame.click('a.geetest_commit'),
+	]);
 });
