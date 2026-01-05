fd6f41d7db71622c8ecd373be9b1d2484f5dde5e
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-01-05 09:49:39 +0800

    Upgrade 2captcha API to v2.

--- bilibili.js
+++ bilibili.js
@@ -25,23 +25,40 @@ browse('https://member.bilibili.com/platform/upload/text/edit', 20, async (page,
 	// GeeTest captcha v3 is required. https://2captcha.com/demo/geetest  https://2captcha.com/api-docs/geetest
 	await frame.waitForSelector('script[src^="https://api.geetest.com/get.php"]');
 	const geetestParams = await frame.$eval('script[src^="https://api.geetest.com/get.php"]', el => el.src.split('?')[1]).then(search => new URLSearchParams(search));
-	const inRes = await fetch(`https://2captcha.com/in.php?key=${process.env.TWOCAPTCHA_API_KEY}&method=geetest&gt=${geetestParams.get('gt')}&challenge=${geetestParams.get('challenge')}&api_server=api.geetest.com&pageurl=https://member.bilibili.com/platform/upload/text/edit&json=1`);
+	const inRes = await fetch('https://api.2captcha.com/createTask', { // https://2captcha.com/api-docs/create-task
+		method: 'POST',
+		body: JSON.stringify({
+			clientKey: process.env.TWOCAPTCHA_API_KEY,
+			task: {
+				type: "GeeTestTaskProxyless",
+				websiteURL: 'https://member.bilibili.com/platform/upload/text/edit',
+				gt: geetestParams.get('gt'),
+				challenge: geetestParams.get('challenge'),
+			},
+		}),
+	});
 	const inRet = await inRes.json();
-	if (!inRet.status) { // status === 0 indicates failure, e.g. when inRet.request === 'ERROR_ZERO_BALANCE' or "ERROR_NO_SLOT_AVAILABLE" or "ERROR_IP_NOT_ALLOWED".
-		throw new Error(`${inRet.request}: ${inRet.error_text}`);
+	if (inRet.errorId) { // errorId > 0 indicates failure, e.g. when inRet.errorCode === 'ERROR_ZERO_BALANCE' or "ERROR_NO_SLOT_AVAILABLE" or "ERROR_IP_NOT_ALLOWED".
+		throw new Error(`Error ${inRet.errorId}: ${inRet.errorCode}: ${inRet.errorDescription}`);
 	}
-	await new Promise(resolve => setTimeout(resolve, 25000)); // Wait for 25 seconds for the captcha to be solved.
+	await new Promise(resolve => setTimeout(resolve, 2000)); // Wait for 25 seconds for the captcha to be solved.
 	const resRet = await new Promise((resolve, reject) => {
 		const polling = setInterval(async () => {
-			const resRes = await fetch(`https://2captcha.com/res.php?key=${process.env.TWOCAPTCHA_API_KEY}&action=get&id=${inRet.request}&json=1`);
+			const resRes = await fetch('https://api.2captcha.com/getTaskResult', { // https://2captcha.com/api-docs/get-task-result
+				method: 'POST',
+				body: JSON.stringify({
+					clientKey: process.env.TWOCAPTCHA_API_KEY,
+					taskId: inRet.taskId,
+				}),
+			});
 			const resRet = await resRes.json();
-			if (resRet.request === 'CAPCHA_NOT_READY') return;
+			if (resRet.status === 'processing') return;
 			clearInterval(polling);
 			resolve(resRet);
-		}, 5000); // If captcha is not solved yet server will return CAPCHA_NOT_READY result. Repeat your request in 5 seconds.
+		}, 5000); // When the task is not complete yet, you receive the following response { "errorId": 0, "status": "processing" }. Wait at least 5 seconds and repeat the request.
 	});
-	if (!resRet.status) { // status === 0 indicates failure, e.g. when resRet.request === 'ERROR_CAPTCHA_UNSOLVABLE'.
-		throw new Error(`${resRet.request}: ${resRet.error_text}`); // e.g. ERROR_CAPTCHA_UNSOLVABLE: Workers could not solve the Captcha
+	if (resRet.errorId) { // errorId > 0 indicates failure, e.g. when resRet.errorCode === 'ERROR_CAPTCHA_UNSOLVABLE'.
+		throw new Error(`Error ${resRet.errorId}: ${resRet.errorCode}: ${resRet.errorDescription}`); // e.g. ERROR_CAPTCHA_UNSOLVABLE: Workers could not solve the Captcha
 	}
 //	Inject these answers [ 'geetest_challenge', 'geetest_validate', 'geetest_seccode' ]
 //	Looks like the answer is returned to the server by creating a new script tag <script src="https://api.geetest.com/ajax.php?gt=&challenge=&w=">
