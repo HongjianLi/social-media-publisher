e52d79cb80d445baa8d0f35385f7dc789eb17217
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-02-18 10:47:00 +0800

    Avoid overriding the date variable.

--- start.bat
+++ start.bat
@@ -1,26 +1,25 @@
 echo off
-set d=%date%
-set date=%d:~6,4%-%d:~3,2%-%d:~0,2%
-echo %date% %time% Script started
+set YYYYMMDD=%date:~6,4%-%date:~3,2%-%date:~0,2%
+echo %YYYYMMDD% %time% Script started
 set PUPPETEER_EXECUTABLE_PATH=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe
-echo %date% %time% node media.js
+echo %YYYYMMDD% %time% node media.js
 node media.js
-echo %date% %time% node login.js
+echo %YYYYMMDD% %time% node login.js
 node login.js
-echo %date% %time% node douyin.js
+echo %YYYYMMDD% %time% node douyin.js
 node douyin.js
-echo %date% %time% node toutiao.js
+echo %YYYYMMDD% %time% node toutiao.js
 node toutiao.js
-echo %date% %time% node kuaishou.js
+echo %YYYYMMDD% %time% node kuaishou.js
 node kuaishou.js
-echo %date% %time% node xiaohongshu.js
+echo %YYYYMMDD% %time% node xiaohongshu.js
 node xiaohongshu.js
-echo %date% %time% node weibo.js
+echo %YYYYMMDD% %time% node weibo.js
 node weibo.js
-echo %date% %time% node baijiahao.js
+echo %YYYYMMDD% %time% node baijiahao.js
 node baijiahao.js
-echo %date% %time% node bilibili.js
+echo %YYYYMMDD% %time% node bilibili.js
 node bilibili.js
-echo %date% %time% node qzone.js
+echo %YYYYMMDD% %time% node qzone.js
 node qzone.js
-echo %date% %time% Script completed
+echo %YYYYMMDD% %time% Script completed
